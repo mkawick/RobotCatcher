270b2ac9c9e812088f5861c6dfe389af66166b8a
mickey kawick <mkawick@yahoo.com>
2020-04-29 06:58:58 +0100

    clickable obstacles now work

--- Assets/EndlessRunner01Scripts/ClickableObstacle.cs
+++ Assets/EndlessRunner01Scripts/ClickableObstacle.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class ClickableObstacle : MonoBehaviour
 {
-    
+    int matchMaterialIndex = 0;
+    public ObstacleManager obstacleManager;
+    Renderer modelRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        modelRenderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    internal void RandomizeIndex()
+    {
+        int num = obstacleManager.GetNumMaterials();
+
+        matchMaterialIndex = Random.Range(0, num);
+        SetupMaterial();
+    }
+    private void OnMouseDown()
+    {
+        int num = obstacleManager.GetNumMaterials();
+        matchMaterialIndex++;
+        if (matchMaterialIndex >= num)
+        {
+            matchMaterialIndex = 0;
+        }
+        SetupMaterial();
+    }
+
+    void SetupMaterial()
+    {
+        Material mat = obstacleManager.GetMaterial(matchMaterialIndex);
+        GetComponent<Renderer>().material = mat;
     }
 }
--- Assets/EndlessRunner01Scripts/ObstacleManager.cs
+++ Assets/EndlessRunner01Scripts/ObstacleManager.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class ObstacleManager : MonoBehaviour
 {
     public GroundChunkManager gcm;
-    public GameObject[] obstacles;
+    public GameObject[] obstaclePrefabs;
     public float currentDifficulty = 1;
+    public Material[] matchMaterials;
+    public GameObject obstacleContainer;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Assert(gcm != null);
+        HidePrefabs();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void HidePrefabs()
+    {
+        foreach(var p in obstaclePrefabs)
+        {
+            p.SetActive(false);
+        }
     }
 
     public void ChunkAdded(GameObject chunk)
@@ -41,19 +52,39 @@ public class ObstacleManager : MonoBehaviour
         float positionMaxZ = center.z + boundsZ;
         for (int i=0; i< numObstaclesToGenerate; i++)
         {
-            int whichObstacle = i % obstacles.Length;
+            int whichObstacle = i % obstaclePrefabs.Length;
             //float height = Random.RandomRange(1, 25);
             float height = 0.68f;
             float angle = Random.Range(0, 85);
 
-            Quaternion q = obstacles[whichObstacle].transform.rotation;
+            Quaternion q = obstaclePrefabs[whichObstacle].transform.rotation;
             q *= Quaternion.Euler(Vector3.up * angle);
             float x = Random.Range(positionMinX, positionMaxX);
             float z = Random.Range(positionMinZ, positionMaxZ);
             Vector3 pos = new Vector3(x, height, z);
-            GameObject newChunk = Instantiate(obstacles[whichObstacle], pos, q );
-            newChunk.SetActive(true);
+            CreateObstacle(whichObstacle, pos, q);
         }
+    }
 
+    void CreateObstacle(int which, Vector3 pos, Quaternion q)
+    {
+        GameObject newChunk = Instantiate(obstaclePrefabs[which], pos, q);        
+        ClickableObstacle obst = newChunk.GetComponent<ClickableObstacle>();
+        if (obst)
+        {
+            obst.obstacleManager = this;
+        }
+        newChunk.SetActive(true);
+        obst.transform.parent = obstacleContainer.transform;
+        obst.RandomizeIndex();
+    }
+
+    internal int GetNumMaterials() { return matchMaterials.Length; }
+    internal Material GetMaterial(int which)
+    {
+        if (which >= matchMaterials.Length)
+            return null;
+
+        return matchMaterials[which];
     }
 }
